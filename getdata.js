31c997a10a191ca7d88e5a368a07f0f1fadeb226
soilniba <soilniba@gmail.com>
2018-08-06 23:43:19 +0800

    添加两个路径的获取

--- getdata.js
+++ getdata.js
@@ -1,54 +1,46 @@
-// var express = require('express')
-// var app = express()
-// var bodyParser = require('body-parser')
 var requestify = require('requestify')
-// var fse = require('fs-extra')
 var jsonfile = require('jsonfile')
-// var moment = require('moment')
 var cheerio = require('cheerio')
-var szJsonPath = './public/data.json'
-jsonfile.readFile(szJsonPath, function(err, obj) {
-	if (err) {
-		console.info('not find save.json \n')
-		jsonfile.writeFile(szJsonPath, [], function (err) {
-			if (err) {
-				console.error(err)
-			} else {
-				main()
-			}
-		})
-	} else {
-		main()
-	}
-	// console.info(err)
-	// console.dir(obj)
+var arrayUrlList = [
+	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=8963&c=-3&o=5&sa=0&p=1', szJsonPath: './public/shuangmeng.json'},
+	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=6897&c=-3&o=5&sa=0&p=1', szJsonPath: './public/weimanxua.json'},
+]
+
+arrayUrlList.forEach(function(tLine){
+	CreateJsonFile(tLine)
 })
 
-function main(){
-    console.info('*** Main Start *** \n')
-    var oneSecond = 1000 * 60 // one second = 1000 x 1 ms
-    setInterval(function() {
-        GetUrl('https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=8963&c=-3&o=5&sa=0&p=1')
-    }, oneSecond)
-    // GetUrl('https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=8963&c=-3&o=5&sa=0&p=1')
-	// var tJson = jsonfile.readFileSync(szJsonPath)
-	// tJson.tA = {}
-	// tJson.arrayB = []
-	// tJson.tA.ccc = 142314
-	// tJson.arrayB.push('aaaaa')
-	// tJson.arrayB.push('bbbbb')
-	// tJson.arrayB.push('ccccc')
-	// tJson.arrayB.pop()
-	// tJson.arrayB.shift()
-	// tJson.push([Date.now() ,108.37])
-	// jsonfile.writeFile(szJsonPath, tJson, function (err) {
-	// 	if (err) {
-	// 		console.error(err)
-	// 	}
-	// })
+function CreateJsonFile(tLine){
+	jsonfile.readFile(tLine.szJsonPath, function(err, obj) {
+		if (err) {
+			console.info('not find json \n')
+			jsonfile.writeFile(tLine.szJsonPath, [], function (err) {
+				if (err) {
+					console.error(err)
+				} else {
+					main(tLine)
+				}
+			})
+		} else {
+			main(tLine)
+		}
+		// console.info(err)
+		// console.dir(obj)
+	})
 }
 
-function GetUrl(szUrl){
+function main(tLine){
+	console.info('*** Main Start *** \n')
+	GetUrl(tLine.szURL, tLine.szJsonPath)
+    // var oneSecond = 1000 * 60 // one second = 1000 x 1 ms
+    // setInterval(function() {
+	// 	arrayUrlList.forEach(function(tLine){
+	// 		GetUrl(tLine.szURL, tLine.szJsonPath)
+	// 	})
+    // }, oneSecond)
+}
+
+function GetUrl(szUrl, szJsonPath){
     //查询服务器状态
     requestify.request(szUrl, {
         method: 'GET',
@@ -74,7 +66,3 @@ function GetUrl(szUrl){
 		console.dir(tResponse)
     });
 }
-
-// app.use('/public', express.static('public'))
-
-// app.listen(8081)
