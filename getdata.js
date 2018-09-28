7df6e8e9acab5bc27ec40b63b667c49621f640fc
soilniba <soilniba@gmail.com>
2018-09-28 10:18:43 +0800

    +蝶服

--- getdata.js
+++ getdata.js
@@ -4,6 +4,7 @@ var cheerio = require('cheerio')
 var arrayUrlList = [
 	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=8963&c=-3&o=5&sa=0&p=1', szJsonPath: './public/shuangmeng.json'},
 	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=150&area=1319&srv=6897&c=-3&o=5&sa=0&p=1', szJsonPath: './public/weimanxua.json'},
+	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=150&area=1315&srv=30493&c=-3&o=5&sa=0&p=1', szJsonPath: './public/dielianhua.json'},
 	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=267&area=2490&srv=29946&c=-3&o=5&sa=0&p=1', szJsonPath: './public/taihuashan.json'},
 	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=267&area=2490&srv=31721&c=-3&o=5&sa=0&p=1', szJsonPath: './public/butianling.json'},
 	{szURL: 'https://www.uu898.com/newTrade.aspx?gm=267&area=2490&srv=31815&c=-3&o=5&sa=0&p=1', szJsonPath: './public/yingxianmen.json'},
